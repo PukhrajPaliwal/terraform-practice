ab4b72bba1d106e4650e774787b4dc5840c6c50f
Pukhraj Paliwal <ppaliwal@yotta.com>
2025-08-02 13:46:56 +0530

    code

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  features {}
+      subscription_id = "a7d5c3b8-efab-4b09-938f-ce6ca000d654"
+           
+  
+}   
---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {   
+    type = string
+    default = "pukhraj2025"
+
+
+  
+}   
+variable "resource_group_location" {
+    type = string
+    default = "gen2-eastus"
+            
+  
+}
