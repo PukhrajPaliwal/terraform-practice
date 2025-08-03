b6971d519534e9674f3fae5c3e245caa5a642673
PukhrajPaliwal <PukhrajPaliwal@gmail.com>
2025-08-03 20:54:38 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
   features {}
-      subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f
-"
+      subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
            
   
 }   
